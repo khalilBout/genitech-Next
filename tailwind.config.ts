aac3620db0d7c1b62963b1993df2b712b4631ffd
khalilBout <bouternikh.khalil@gmal.com>
2025-03-10 22:42:04 +0100

    ferst commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,38 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+      fontFamily: {
+        Title: ["Cairo", "sans-serif"],
+        Body: ["ElMessiri", "sans-serif"],
+      },
+      colors: {
+        primary: "#19C4F9",
+        primaryDark: "#87131d",
+        lightOrange: "#b07155",
+        darkGray: "#1a1f25",
+        lightGray: "#272c35",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+        },
       },
     },
   },
