18cd835a96cc5bf3eb629902333627338b92e2d1
khalilBout <bouternikh.khalil@gmal.com>
2025-03-17 23:06:52 +0100

    fix moer

--- app/[locale]/login/page.jsx
+++ app/[locale]/login/page.jsx
@@ -6,14 +6,12 @@ import { redirect } from "next/navigation";
 
 const page = async () => {
   const session = await getServerSession();
-  if (session) {
-    // التحقق مما إذا كان المستخدم أدمن أم لا
-    if (session.user.role === "admin") {
-      redirect("/dashboard"); // توجيه الأدمن إلى لوحة التحكم
-    } else {
-      redirect("/"); // توجيه المستخدم العادي إلى الصفحة الرئيسية
-    }
-  }
+  // if (session) {
+  //   // التحقق مما إذا كان المستخدم أدمن أم لا
+  //   if (session.user.role !== "admin") {
+  //     redirect("/"); // توجيه الأدمن إلى لوحة التحكم
+  //   }
+  // }
   return (
     <div className="screenPadding w-screen flex justify-center items-center ">
       <Login />
