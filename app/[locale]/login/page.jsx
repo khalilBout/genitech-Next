976c7f098071dce2a47123d872b6f06693e134fd
khalilBout <bouternikh.khalil@gmal.com>
2025-03-17 23:00:21 +0100

    fix bag

--- app/[locale]/login/page.jsx
+++ app/[locale]/login/page.jsx
@@ -7,7 +7,12 @@ import { redirect } from "next/navigation";
 const page = async () => {
   const session = await getServerSession();
   if (session) {
-    redirect("/?callbackUrl=/login");
+    // التحقق مما إذا كان المستخدم أدمن أم لا
+    if (session.user.role === "admin") {
+      redirect("/dashboard"); // توجيه الأدمن إلى لوحة التحكم
+    } else {
+      redirect("/"); // توجيه المستخدم العادي إلى الصفحة الرئيسية
+    }
   }
   return (
     <div className="screenPadding w-screen flex justify-center items-center ">
