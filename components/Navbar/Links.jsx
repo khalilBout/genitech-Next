fe6536e0d50e7e44a1c58c06148dc8ec3b79fd2f
khalilBout <bouternikh.khalil@gmal.com>
2025-03-27 01:18:12 +0100

    fix navbar oji

--- components/Navbar/Links.jsx
+++ components/Navbar/Links.jsx
@@ -1,49 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import LinkService from "./LinkService";
 import LocalizedLink from "../Ui/LocalizedLink";
 
 const Links = ({ items }) => {
-  const [openLinkService, setOpenLinkService] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const timerRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (timerRef.current) clearTimeout(timerRef.current);
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    timerRef.current = setTimeout(() => setIsHovered(false), 500); // تأخير 500ms قبل الإخفاء
+  };
 
   return (
-    <div className=" hidden mdl:block relative">
+    <div className="hidden mdl:block relative">
       <ul className="flex justify-center items-center gap-4 font-Title font-bold">
-        <li className="">
+        <li>
           <a href={items[0].link}>{items[0].name}</a>
         </li>
-
-        <li className="">
+        <li>
           <LocalizedLink href={items[1].link}>{items[1].name}</LocalizedLink>
         </li>
 
-        <li className=" relative group">
-          {/* <button
-            className=""
-            onClick={() => setOpenLinkService(!openLinkService)}
-          > */}
+        <li
+          className="relative group cursor-pointer"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           {items[2].name}
-          <div
-            // className="hidden group-hover:block absolute top-[40px] w-[120vw] left-1/2 transform -translate-x-1/2 bg-[#CEEBFD]"
-            className=" hidden group-hover:block absolute top-[40px] w-[120vw] left-1/2 transform -translate-x-1/2 bg-[#CEEBFD] transition-all duration-300 delay-200 group-hover:delay-100"
-          >
-            <LinkService setOpenLinkService={setOpenLinkService} />
-          </div>
-          {/* </button> */}
+          {isHovered && (
+            <div className="absolute top-[35px] w-[120vw] left-1/2 transform -translate-x-1/2 bg-[#CEEBFD] transition-all duration-300">
+              <LinkService />
+            </div>
+          )}
         </li>
-
-        <li className="">
+        <li>
           <LocalizedLink href={items[3].link}>{items[3].name}</LocalizedLink>
         </li>
-        <li className="">
+        <li>
           <a href={items[4].link}>{items[4].name}</a>
         </li>
       </ul>
-
-      {/* {openLinkService && (
-        <div className=" absolute top-[80px] w-screen left-1/2 transform -translate-x-1/2 bg-[#CEEBFD]">
-          <LinkService setOpenLinkService={setOpenLinkService} />
-        </div>
-      )} */}
     </div>
   );
 };
